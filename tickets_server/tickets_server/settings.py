f09576b2b76c516ac7f96bec3eeb6bc262baa209
bengroneman <bluegroneman10@gmail.com>
2022-02-09 16:52:55 +0000

    cleaned key out

--- tickets_server/tickets_server/settings.py
+++ tickets_server/tickets_server/settings.py
@@ -8,8 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c_)ffwdv3nbde6ftj=-(uy=o$1xgsq1ex7xf33^9heqhbpa14c'
-
+with open('/home/swordOfVim/secret.txt') as f:
+    SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
