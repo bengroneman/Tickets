4729f77b324fbff4678e8e514ae09dec0234cac6
Ben Groneman <bluegroneman10@gmail.com>
2022-02-09 12:26:37 -0700

    fix: static assets in production

--- tickets_server/tickets_server/settings.py
+++ tickets_server/tickets_server/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = "/home/swordOfVim/code/Tickets/tickets_server/static"
+STATIC_ROOT = "/home/swordOfVim/Tickets/tickets_server/static"
 
 STATIC_URL = 'static/'
 
